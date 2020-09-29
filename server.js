debe8ae67318c974011176989f359ccc9cda432e
shawnagohel <shawna.gohel@gmail.com>
2020-09-28 20:05:06 -0700

    update port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { animals } = require('./data/animals');
 
 
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -68,9 +69,8 @@ function filterByQuery(query, animalsArray) {
 //   });
 
 
-
-
-
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
+
+
