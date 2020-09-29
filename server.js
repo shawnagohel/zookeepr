7b5a4e5753993b0e345d0dd60cb6bc1a0d92ed94
shawnagohel <shawna.gohel@gmail.com>
2020-09-28 20:28:46 -0700

    update json

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -52,22 +58,14 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-
-
-// app.get('/api/animals', (req, res) => {
-//     let results = animals;
-//     console.log(req.query)
-//     res.json(results);
-//   });
-
-
-
-
-
-// app.get('/api/animals', (req, res) => {
-//     res.json(animals);
-//   });
-
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
